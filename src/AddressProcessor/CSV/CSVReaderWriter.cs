3c05326c2980d7572a2908f7d990612c207e559e
Arijus Gilbrantas <arijusg@gmail.com>
2016-12-05 11:56:11 +0000

    Added comments/trafe offs to the top of the file, incase they are missed from readme.txt

--- src/AddressProcessor/CSV/CSVReaderWriter.cs
+++ src/AddressProcessor/CSV/CSVReaderWriter.cs
@@ -5,6 +5,17 @@ namespace AddressProcessing.CSV
     /*
         2) Refactor this class into clean, elegant, rock-solid & well performing code, without over-engineering.
            Assume this code is in production and backwards compatibility must be maintained.
+
+        Trade offs/comments
+
+      - I have not implemented my own csv reader, it would have been wasteful. CsvHelper is high quality open source library.
+      - I tested CSVReaderWriter functionality, not implementation.
+      - "Unknown file mode for check and throws exception" - not relevant as we passing in a enum. If enum gets more values, tests should be written around it. We do not code defensively..
+      - Method Read(string column, string column2) is redundant, string is immutable type
+      - I did not implement IDisposable as it can't be used using current design.
+      - Reading returns two columns only - not flexible implementation. Should return column array or be mapped to objects
+      - Exception handling is left for AddressFileProcessor.
+
     */
 
     public class CSVReaderWriter
